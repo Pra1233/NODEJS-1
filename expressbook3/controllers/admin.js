b58e3aee34b16f731b164bcbab58ebb2b9fbc378
Prabhat <103235389+Pra1233@users.noreply.github.com>
2023-08-05 16:31:10 +0530

    Done

--- expressbook3/controllers/admin.js
+++ expressbook3/controllers/admin.js
@@ -1,12 +1,10 @@
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
-  res.render('admin/add-product', {
+  res.render('admin/edit-product', { //add-[roduct]
     pageTitle: 'Add Product',
     path: '/admin/add-product',
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true
+    editing:false, //edit
   });
 };
 
@@ -15,10 +13,42 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(title, imageUrl, description, price);
+  const product = new Product(null,title, imageUrl, description, price);
   product.save();
-  res.redirect('/');
+  res.redirect('/'); 
 };
+
+exports.getEditProduct = (req, res, next) => {//2
+  const editMode=req.query.edit; //query parameter url give value extracted value is
+   //" true"  (if edit mode empty than false)
+   if(!editMode){
+    return res.redirect('/');
+   }
+   const id=req.params.productId;   // line 15 route
+   Product.findById(id,product=>{
+    if (!product) {
+      return res.redirect('/');
+    }
+    res.render('admin/edit-product', { //  show file
+      pageTitle: 'Edit Product', //passing to ejs
+      path: '/admin/edit-product',
+      editing:editMode,
+      product:product,
+    });
+   })
+};
+
+exports.postEditProduct = (req, res, next) => {
+//create new product remove existing product
+const prodId=req.body.productId; //hidden in ejs
+const updatedTitle=req.body.title;
+const updatedPrice=req.body.price;
+const updatedImageUrl=req.body.imageUrl;
+const updatedDescription =req.body.description;
+const updatedProduct=new Product(prodId,)
+
+
+}; 
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
